8de63d225f35ee1d49ddaaf2cc9d8fc1600b0ffc
Paulavera-dot <fernandacaceres451@gmail.com>
2026-05-08 16:26:33 -0500

    cambios #9.3

--- conexion.php
+++ conexion.php
@@ -3,9 +3,8 @@
 $sever = "localhost";
 $user = "root";
 $pass = "";
-=======
 $db = "sistema";
->>>>>>> 6437cdfe83b7100765bb5d6a82355bc79986ee40
+
 
 $conexion = new mysqli($server; $user, $pass, $db);
 
